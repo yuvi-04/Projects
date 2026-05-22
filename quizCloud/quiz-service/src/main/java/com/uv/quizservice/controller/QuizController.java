c1d4dc0a778b97b1d48b3f1e1a6a0cde56cec641
Yuvraj Budhiraja <budhirajayuvraj4@gmail.com>
2026-05-22 17:52:17 +0530

    FIX: bootstrap.properties|zipkin armeria server config|path for config-server|dependency added for question and quiz service

--- quizCloud/quiz-service/src/main/java/com/uv/quizservice/controller/QuizController.java
+++ quizCloud/quiz-service/src/main/java/com/uv/quizservice/controller/QuizController.java
@@ -4,6 +4,7 @@ import com.uv.quizservice.model.QuestionWrapper;
 import com.uv.quizservice.model.QuizDto;
 import com.uv.quizservice.model.Response;
 import com.uv.quizservice.service.QuizService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("quiz")
+@Slf4j
 public class QuizController {
 
     @Autowired
@@ -19,16 +21,19 @@ public class QuizController {
 
     @PostMapping("create")
     public ResponseEntity<String> createQuiz(@RequestBody QuizDto quizDto){
+        log.info("Creating quiz title={} category={} questions={}", quizDto.getTitle(), quizDto.getCategoryName(), quizDto.getNumQuestions());
         return quizService.createQuiz(quizDto.getCategoryName(), quizDto.getNumQuestions(), quizDto.getTitle());
     }
 
     @PostMapping("get/{id}")
     public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable Integer id){
+        log.info("Fetching quiz questions for quizId={}", id);
         return quizService.getQuizQuestions(id);
     }
 
     @PostMapping("submit/{id}")
     public ResponseEntity<Integer> submitQuiz(@PathVariable Integer id, @RequestBody List<Response> responses){
+        log.info("Submitting quizId={} with {} responses", id, responses.size());
         return quizService.calculateResult(id, responses);
     }
 
--- quizCloud/zipkin-server/src/main/java/com/uv/zipkinserver/ZipkinServerApplication.java
+++ quizCloud/zipkin-server/src/main/java/com/uv/zipkinserver/ZipkinServerApplication.java
@@ -2,9 +2,10 @@ package com.uv.zipkinserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import zipkin.server.internal.RegisteredComponent;
+import zipkin2.server.internal.EnableZipkinServer;
 
 @SpringBootApplication
+@EnableZipkinServer
 public class ZipkinServerApplication {
 
 	public static void main(String[] args) {
