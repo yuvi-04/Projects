ad78f21be9e0557b04fd413466519cb5cae4dbf7
Yuvraj Budhiraja <budhirajayuvraj4@gmail.com>
2026-06-03 14:17:30 +0530

    removed quizCoud to another repo to add Jenkins

--- quizCloud/quiz-service/src/main/java/com/uv/quizservice/dao/QuizDao.java
+++
@@ -1,8 +0,0 @@
-package com.uv.quizservice.dao;
-
-
-import com.uv.quizservice.model.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuizDao extends JpaRepository<Quiz,Integer> {
-}
